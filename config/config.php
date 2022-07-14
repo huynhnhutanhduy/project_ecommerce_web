199d471fc3bf7724db108c49196ecc4387bad6b2
huynhnhutanhduy <duy.hnad@gmail.com>
2022-07-14 23:40:28 +0700

    uploade source code web final IS207

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Database configurations 
+define("HOST", "localhost");
+define("USER", "root");
+define("DATABASE","mp");
+define("PASSWORD","");
+define("PASSWORDADMIN","admin");
+
+// Base URL
+define("BASEURL", "/-website-mp");
+
+?>
